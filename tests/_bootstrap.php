8f709a2a927ee4120f1aa82be69cd0747c1f4f02
davert <davert.githole@mailican.com>
2013-08-01 06:39:08 +0300

    bootstrap cleaned

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,26 +1,16 @@
 <?php
 define('YII_ENV', 'test');
-// This is global bootstrap for autoloading 
-// defined('YII_DEBUG') or define('YII_DEBUG', true);
-
-//require(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
-
 
 $kernel = AspectMock\Kernel::getInstance();
 $kernel->init([
     'debug' => true,
-    'cacheDir' => __DIR__.'/_data/cache',
     'includePaths' => [__DIR__.'/../common'],
-//    'excludePaths' => [__DIR__.'/../vendor'],
 ]);
-//$kernel->loadFile(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
-require __DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php';
-$kernel->loadPhpFiles(__DIR__.'/../common');
+$kernel->loadFile(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
 
 $config = yii\helpers\ArrayHelper::merge(
 	require(__DIR__ . '/../frontend/config/main.php'),
 	require(__DIR__ . '/../frontend/config/main-local.php')
 );
 
-//new \common\models\LoginForm();vj;t
 $application = new yii\web\Application($config);
